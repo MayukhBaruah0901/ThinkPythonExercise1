78732eadecc9a145b648fb8b625af8b67a817928
MayukhBaruah0901 <160479018+MayukhBaruah0901@users.noreply.github.com>
2024-02-19 23:34:36 +0530

    Revert "exercise"
    This reverts commit 4db099c20b659ea6a2bbec89f9eef77c0637a597.

--- 2.1.py
+++
@@ -1,6 +0,0 @@
-minute=42
-seconds=42
-total =minute*60+seconds
-print("There are",total_seconds,"in",minute,"minute and",seconds,"seconds")
-
-#total_seconds=2562
--- 2.2.py
+++
@@ -1,5 +0,0 @@
-kilometers=10
-miles=kilometers/1.61
-print("Total miles =",miles)
-
-#There are 16.1 miles in 10 kilometers
