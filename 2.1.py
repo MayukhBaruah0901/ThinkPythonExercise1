4db099c20b659ea6a2bbec89f9eef77c0637a597
MayukhBaruah0901 <160479018+MayukhBaruah0901@users.noreply.github.com>
2024-02-19 23:33:41 +0530

    exercise

---
+++ 2.1.py
@@ -0,0 +1,6 @@
+minute=42
+seconds=42
+total =minute*60+seconds
+print("There are",total_seconds,"in",minute,"minute and",seconds,"seconds")
+
+#total_seconds=2562
---
+++ 2.3.py
@@ -0,0 +1,9 @@
+kilometer=10
+minute=42
+seconds=42
+miles=kilometer/1.61
+minute_h=42/60
+second_h=42/3600
+total_hour=minute_h+second_h
+avg_speed=miles/total_hour
+print("The average speed is ",avg_speed)
